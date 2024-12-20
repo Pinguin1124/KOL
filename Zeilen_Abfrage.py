34a7ff05212e5ab0517367cd78cbeb56967be7f3
Pinguin1124 <Raphael_Herrmann_00000@outlook.de>
2024-12-20 17:36:55 +0100

    Update Zeilen_Abfrage.py

--- Zeilen_Abfrage.py
+++ Zeilen_Abfrage.py
@@ -3,17 +3,26 @@ import mysql.connector
 # Verbindung zur Datenbank herstellen
 mydb = mysql.connector.connect(
   host="localhost",  # Ersetze durch deinen Hostnamen
-  user="yourusername",  # Ersetze durch deinen Benutzernamen
-  password="yourpassword",  # Ersetze durch dein Passwort
-  database="yourdatabase"  # Ersetze durch den Namen deiner Datenbank
+  user="Python",  # Ersetze durch deinen Benutzernamen
+  password="KOLSQL",  # Ersetze durch dein Passwort
+  database="kol"  # Ersetze durch den Namen deiner Datenbank
 )
 
 mycursor = mydb.cursor()
 
-# SQL-Abfrage ausführen
-mycursor.execute("SELECT * FROM yourtable LIMIT 1")
+# Wert für LIMIT während der Laufzeit eingeben
+limit_value = int(input("Bitte gib deine ID ein: "))
+
+# SQL-Abfrage mit LIMIT-Variable ausführen
+query = "SELECT * FROM inhalt LIMIT %s"
+mycursor.execute(query, (limit_value,))
 
 # Ergebnis abrufen
-myresult = mycursor.fetchone()
+myresult = mycursor.fetchall()
 
-print(myresult)
+for row in myresult:
+    print(row)
+
+print("Der letzte Wert entspricht der Bindungsenergie!")
+
+input("Zum Beenden Enter drücken!")
